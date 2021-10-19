8abd7dd5f0334de5324768e2b50b38d672c2c7c4
Joshua Santos <joshua.santos1@students.mq.edu.au>
2021-10-19 11:04:42 +1100

    UI Improvements: Removed progress bar, made window smaller, aligned frames better

--- YouTubeDL.py
+++ YouTubeDL.py
@@ -53,7 +53,7 @@ def start():
 
 
 window_width = 500
-window_height = 400
+window_height = 150
 
 window = tk.Tk()
 window.minsize(window_width, window_height)
@@ -63,12 +63,11 @@ main_frame = tk.Frame(window)
 url_frame = tk.Frame(main_frame)
 options_frame = tk.Frame(main_frame)
 quality_frame = tk.Frame(main_frame)
-progress_frame = tk.Frame(main_frame)
-empty_frame = tk.Frame(window, width=window_width, height=200)
+#empty_frame = tk.Frame(window, width=window_width, height=200)
 menu_button_frame = tk.Frame(window)
 
 main_frame.grid()
-empty_frame.grid(row=4, column=0, )
+#empty_frame.grid(row=4, column=0)
 
 url_frame.grid(padx=10, pady=10, sticky='w')
 url_lbl = tk.Label(url_frame, text="URL:")
@@ -84,27 +83,21 @@ dl_audio = tk.Checkbutton(options_frame, text='Download Audio', variable=aud_boo
 dl_vid.grid(row=1)
 dl_audio.grid(row=2, pady=10)
 
-quality_frame.grid(row=1, column=0, padx=50, sticky='e')
+quality_frame.grid(row=1, column=0, padx=10, sticky='e')
 vid_string = tk.StringVar(quality_frame, 'Select Quality...')
 aud_string = tk.StringVar(quality_frame, 'Select Quality...')
 audq_cmb = ttk.Combobox(quality_frame, textvariable=vid_string, state='readonly', width=40)
 vidq_cmb = ttk.Combobox(quality_frame, textvariable=aud_string, state='readonly', width=40)
-vidq_cmb.grid(row=1, column=1, sticky='e')
+vidq_cmb.grid(row=1, column=1)
 audq_cmb.grid(row=2, column=1, pady=10)
 
-progress_frame.grid(row=2, column=0, sticky='w', padx=15)
-prog_lbl = tk.Label(progress_frame, text='Progress:')
-prog_bar = ttk.Progressbar(progress_frame, orient='horizontal', length=450, mode='determinate')
-prog_lbl.grid(row=2, column=0, sticky='w')
-prog_bar.grid(row=3, column=0)
-
-menu_button_frame.grid(row=5, column=0, sticky='e')
+menu_button_frame.grid(row=5, column=0, padx=10, sticky='e')
 format_btn = ttk.Button(menu_button_frame, text='Get Formats', command=get_formats)
 close_btn = ttk.Button(menu_button_frame, text='Close', command=close)
 dl_btn = ttk.Button(menu_button_frame, text='Download', command=download)
 format_btn.grid(row=5, column=0)
 dl_btn.grid(row=5, column=1, padx=10)
-close_btn.grid(row=5, column=2, padx=10)
+close_btn.grid(row=5, column=2)
 
 window.mainloop()
 
