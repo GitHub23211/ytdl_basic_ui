bfb7546eb613d237c4cff2e9bfc83140e1973ace
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-19 20:04:28 +1100

    moved state-related code up into the state class Added label to indicate song dir

--- ui.py
+++ ui.py
@@ -1,14 +1,10 @@
-from tkinter import ttk, Frame, Label, Entry, Button, Listbox, Scrollbar, StringVar, DoubleVar, messagebox, filedialog, ttk
+from tkinter import ttk, Frame, Label, Entry, Button, Listbox, Scrollbar, ttk
 import datetime
 
 class UI(Frame):
     def __init__(self, root, state):
         Frame.__init__(self, root)
         self.state = state
-        self.url_var = StringVar(value='')
-        self.prog_title = StringVar(value='Ready to Download')
-        self.prog_var = DoubleVar(value=0.0)
-        self.song_queue = StringVar(value=state.queue.get())
         root.columnconfigure(0, weight=1)
     
     def build(self):
@@ -21,7 +17,7 @@ class UI(Frame):
     def url_entry(self):
         f = Frame(self)
         url_lbl = Label(f, text='URL:')
-        url_ent = Entry(f, textvariable=self.url_var, width=50)
+        url_ent = Entry(f, textvariable=self.state.url_var, width=50)
 
         f.grid(padx=10, pady=20)
         url_lbl.grid(row=0, column=0)
@@ -30,28 +26,33 @@ class UI(Frame):
     def buttons(self):
         f = Frame(self)
 
-        add_song = Button(f, text='Add to Queue', command=self.add_song)
-        download = Button(f, text='Start Download')
+        add_song = Button(f, text='Add to Queue', command=self.state.add_song)
+        download = Button(f, text='Start Download', command=self.state.start)
+        change_dir = Button(f, text='Change Directory', command=self.state.change_dir)
         cancel = Button(f, text='Cancel')
 
         f.grid()
         add_song.grid(row=0, column=0, padx=10, sticky=('w', 'e'))
         download.grid(row=0, column=1, sticky=('w', 'e'))
-        cancel.grid(row=0, column=2, padx=10, sticky=('w', 'e'))
+        change_dir.grid(row=0, column=2, padx=10, sticky=('w', 'e'))
+        cancel.grid(row=0, column=3, sticky=('w', 'e'))
     
     def progress_bar(self):
         f = Frame(self)
-        t = Label(f, textvariable=self.prog_title)
-        p = ttk.Progressbar(f, orient='horizontal', length=300, mode='determinate', variable=self.prog_var, value=self.prog_var.get())
+        t = Label(f, textvariable=self.state.prog_title)
+        d = Label(f, textvariable=self.state.cur_dir)
+        p = ttk.Progressbar(f, orient='horizontal', length=300, mode='determinate', variable=self.state.prog_var, value=self.state.prog_var.get())
+        
 
         f.grid(pady=20)
         t.grid()
-        p.grid()
+        p.grid(pady=5)
+        d.grid(pady=5)
     
     def song_list(self):
         f = Frame(self)
         title = Label(f, text='Song Queue')
-        lbox = Listbox(f, width=61, listvariable=self.song_queue, selectmode='single', state='disabled')
+        lbox = Listbox(f, width=61, listvariable=self.state.song_queue, selectmode='single', state='disabled')
         scroll = Scrollbar(f, orient='vertical', command=lbox.yview)
         lbox.configure(yscrollcommand=scroll.set)
 
@@ -59,10 +60,3 @@ class UI(Frame):
         title.grid(row=0, column=0)
         lbox.grid(row=1, column=0)
         scroll.grid(row=1, column=1, sticky=('n', 's'))
-    
-    def add_song(self):
-        url = self.url_var.get()
-        if len(url) > 0:
-            self.state.queue.add(url)
-        print(self.state.queue.get())
-        self.url_var.set('')
