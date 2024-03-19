c4d92829aa96a015ee77be3590a8e2e289c9554d
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-19 17:09:49 +1100

    resized widgets and window,changed y paddings

--- ui.py
+++ ui.py
@@ -8,6 +8,7 @@ class UI(Frame):
         self.prog_title = StringVar(value='Ready to Download')
         self.prog_var = DoubleVar(value=0.0)
         self.song_queue = StringVar(value=state.queue.get())
+        root.columnconfigure(0, weight=1)
     
     def build(self):
         self.grid()
@@ -19,9 +20,9 @@ class UI(Frame):
     def url_entry(self):
         f = Frame(self)
         url_lbl = Label(f, text='URL:')
-        url_ent = Entry(f, textvariable=self.url_var, width=70)
+        url_ent = Entry(f, textvariable=self.url_var, width=50)
 
-        f.grid(padx=10, pady=10)
+        f.grid(padx=10, pady=20)
         url_lbl.grid(row=0, column=0)
         url_ent.grid(row=0, column=1)
     
@@ -32,7 +33,7 @@ class UI(Frame):
         download = Button(f, text='Start Download')
         cancel = Button(f, text='Cancel')
 
-        f.grid(pady=20)
+        f.grid()
         add_song.grid(row=0, column=0, padx=10, sticky=('w', 'e'))
         download.grid(row=0, column=1, sticky=('w', 'e'))
         cancel.grid(row=0, column=2, padx=10, sticky=('w', 'e'))
@@ -42,7 +43,7 @@ class UI(Frame):
         t = Label(f, textvariable=self.prog_title)
         p = ttk.Progressbar(f, orient='horizontal', length=300, mode='determinate', variable=self.prog_var, value=self.prog_var.get())
 
-        f.grid()
+        f.grid(pady=20)
         t.grid()
         p.grid()
 
@@ -50,7 +51,7 @@ class UI(Frame):
     def song_list(self):
         f = Frame(self)
         title = Label(f, text='Song Queue')
-        lbox = Listbox(f, width=60, listvariable=self.song_queue, selectmode='single', state='disabled')
+        lbox = Listbox(f, width=61, listvariable=self.song_queue, selectmode='single', state='disabled')
         scroll = Scrollbar(f, orient='vertical', command=lbox.yview)
         lbox.configure(yscrollcommand=scroll.set)
 
