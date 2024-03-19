13aedae093c261a446e7092f2945a5471a6d84ff
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-19 17:04:00 +1100

    redid UI

--- ui.py
+++ ui.py
@@ -1,163 +1,60 @@
-import yt_dlp
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
+from tkinter import ttk, Frame, Label, Entry, Button, Listbox, Scrollbar, StringVar, DoubleVar, messagebox, filedialog, ttk
 import datetime
-from threading import Thread
-from song_queue import SongQueue
-import re
 
-
-def close():
-    window.destroy()
-
-song_queue = SongQueue()
-
-def download():
-    x = Thread(target=start)
-    x.daemon = True
-    x.start()
-
-def get_dir():
-    directory = filedialog.askdirectory()
-    return directory
-
-def show_progress(dict):
-    if(dict['status'] == 'downloading'):
-        eta = 'Calculating...'
-        name_var.set(f'Downloading: {dict["info_dict"]["title"]}')
-        if(dict["eta"]):
-            eta = str(datetime.timedelta(seconds=dict["eta"]))
-        progress_var.set('Time Left: ' + eta)
-    if(dict['status'] == 'finished'): 
-        song_queue.remove()
-        song_var.set(song_queue.to_string())
-
-
-def pp_progress(dict):
-    if(dict['status'] == 'started'):
-        progress_var.set('Converting to mp3...')
-
-def add_queue(url):
-    if('https://www.youtube.com/watch' in url):
-        song_to_add = re.search("^https[^&]+", url).group()
-        song_queue.add(song_to_add)
-        song_var.set(song_queue.to_string())
-        url_txt.delete('0', 'end')
-
-def start():
-    if url_txt.get():
-        add_queue(url_txt.get())
+class UI(Frame):
+    def __init__(self, root, state):
+        Frame.__init__(self, root)
+        self.url_var = StringVar(value='')
+        self.prog_title = StringVar(value='Ready to Download')
+        self.prog_var = DoubleVar(value=0.0)
+        self.song_queue = StringVar(value=state.queue.get())
     
-    filepath = get_dir()
-    progress_var.set("")
+    def build(self):
+        self.grid()
+        self.url_entry()
+        self.buttons()
+        self.progress_bar()
+        self.song_list()
 
-    if(filepath):
-        ydl_opts = {
-            'format': 'm4a/bestaudio/best',
-            'paths': {'home': filepath},
-            'progress_hooks': [show_progress],
-            'postprocessor_hooks': [pp_progress],
-            'quiet': True,
-            # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-            'postprocessors': [{  # Extract audio using ffmpeg
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3'
-            }]
-        }
+    def url_entry(self):
+        f = Frame(self)
+        url_lbl = Label(f, text='URL:')
+        url_ent = Entry(f, textvariable=self.url_var, width=70)
 
-        if song_queue.hasNext() == True:
-            name_var.set('Starting download...')
-            dl_btn['state'] = 'disabled'
+        f.grid(padx=10, pady=10)
+        url_lbl.grid(row=0, column=0)
+        url_ent.grid(row=0, column=1)
+    
+    def buttons(self):
+        f = Frame(self)
 
-            try:
-                while(song_queue.hasNext()):
-                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                        error_code = ydl.download(list(song_queue.get()))
-                messagebox.showinfo("Complete", "Downloaded all files!")
-            except Exception as e:
-                messagebox.showerror('Error', f'Error has occurred! \n {e} with error_code {error_code}')
+        add_song = Button(f, text='Add to Queue')
+        download = Button(f, text='Start Download')
+        cancel = Button(f, text='Cancel')
 
-            name_var.set('')
-            progress_var.set("Finished!")
-            song_var.set('')
-            dl_btn['state'] = 'normal'
-            song_queue.clear()
+        f.grid(pady=20)
+        add_song.grid(row=0, column=0, padx=10, sticky=('w', 'e'))
+        download.grid(row=0, column=1, sticky=('w', 'e'))
+        cancel.grid(row=0, column=2, padx=10, sticky=('w', 'e'))
+    
+    def progress_bar(self):
+        f = Frame(self)
+        t = Label(f, textvariable=self.prog_title)
+        p = ttk.Progressbar(f, orient='horizontal', length=300, mode='determinate', variable=self.prog_var, value=self.prog_var.get())
 
+        f.grid()
+        t.grid()
+        p.grid()
 
-def make_menu(w):
-    global the_menu
-    the_menu = tk.Menu(w, tearoff=0)
-    the_menu.add_command(label="Cut")
-    the_menu.add_command(label="Copy")
-    the_menu.add_command(label="Paste")
-    the_menu.add_command(label="Select All")
+    
+    def song_list(self):
+        f = Frame(self)
+        title = Label(f, text='Song Queue')
+        lbox = Listbox(f, width=60, listvariable=self.song_queue, selectmode='single', state='disabled')
+        scroll = Scrollbar(f, orient='vertical', command=lbox.yview)
+        lbox.configure(yscrollcommand=scroll.set)
 
-def show_menu(e):
-    w = e.widget
-    the_menu.entryconfigure("Cut",
-    command=lambda: w.event_generate("<<Cut>>"))
-    the_menu.entryconfigure("Copy",
-    command=lambda: w.event_generate("<<Copy>>"))
-    the_menu.entryconfigure("Paste",
-    command=lambda: w.event_generate("<<Paste>>"))
-    the_menu.entryconfigure("Select All",
-    command=lambda: w.event_generate("<<SelectAll>>"))             
-    the_menu.tk.call("tk_popup", the_menu, e.x_root, e.y_root)
-
-# def bar():
-#     progress_bar['value'] += 20
-
-window_width = 500
-window_height = 70
-
-window = tk.Tk()
-window.minsize(window_width, window_height)
-window.title("YouTubeDL")
-
-make_menu(window)
-
-main_frame = tk.Frame(window)
-url_frame = tk.Frame(main_frame)
-menu_button_frame = tk.Frame(main_frame)
-progress_frame = tk.Frame(main_frame)
-queue_frame = tk.Frame(main_frame)
-
-main_frame.grid()
-
-url_frame.grid(padx=10, pady=10, sticky='w')
-url_lbl = tk.Label(url_frame, text="URLS:")
-url_txt = tk.Entry(url_frame, width=70)
-url_lbl.grid(row=0, column=0)
-url_txt.grid(row=0,column=1, ipady=5)
-
-menu_button_frame.grid(row=1, column=0, padx=10, pady=10)
-close_btn = tk.Button(menu_button_frame, text='Cancel', command=close)
-dl_btn = tk.Button(menu_button_frame, text='Download', command=lambda: download())
-q_btn = tk.Button(menu_button_frame, text='Add to Queue', command=lambda: add_queue(url_txt.get()))
-q_btn.grid(row=1, column=1, padx=10)
-dl_btn.grid(row=1, column=2, padx=10)
-close_btn.grid(row=1, column=3)
-
-
-progress_var = tk.StringVar(progress_frame, '', )
-name_var = tk.StringVar(progress_frame, '', )
-song_var = tk.StringVar(queue_frame, '')
-#progress_bar = ttk.Progressbar(progress_frame, orient='horizontal', length=300, mode='determinate')
-
-progress_frame.grid(row=2, column=0, padx=10, pady=10)
-name_lbl = tk.Label(progress_frame, textvariable=name_var)
-name_lbl.grid(row=0, column=0)
-prg_lbl = tk.Label(progress_frame, textvariable=progress_var)
-prg_lbl.grid(row=1, column=0)
-#progress_bar.grid(row=2, column=0)
-
-queue_frame.grid(row=3, column=0, padx=10, pady=10)
-q_lbl = tk.Label(queue_frame, text='Song Queue')
-q_lbl.grid(row=0, column=0)
-songq_lbl = tk.Label(queue_frame, textvariable=song_var)
-songq_lbl.grid(row=1, column=0)
-
-url_txt.bind_class("Entry", "<Button-3><ButtonRelease-3>", show_menu)
-
-window.mainloop()
+        f.grid(pady=20)
+        title.grid(row=0, column=0)
+        lbox.grid(row=1, column=0)
+        scroll.grid(row=1, column=1, sticky=('n', 's'))
